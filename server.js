aed0744098bae8ca9834c09d258ef78e6df8826f
Chen-Hui Chang <j4demacia@gmail.com>
2017-03-21 06:34:22 +0000

    timestamp api

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var urlencode = require('urlencode');
+var moment = require('moment');
 
 var server = express();
 
-server.get('/', function(req,res) {
-    res.end('Hello world!');
+server.get('/:str', function(req,res) {
+    var infoDecoded = urlencode.decode(req.params.str);
+    var time, natural;
+    if (/^[0-9]+$/.test(infoDecoded)) {
+        time = new Date(infoDecoded * 1000);
+        natural = moment(time).format('MMMM D, YYYY');
+        res.send({unix: Number(infoDecoded), natural: natural});
+    } else {
+        time = new Date(infoDecoded).valueOf()/1000;
+        res.send({unix: time, natural: infoDecoded});
+    }
 })
 server.listen(8080, function(){
     console.log('server is running');
