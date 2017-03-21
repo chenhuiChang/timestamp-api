eab4e0a6dd67697b4db064abc24095536d64a1b0
Chen-Hui Chang <j4demacia@gmail.com>
2017-03-21 07:01:15 +0000

    more precise regex case

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var moment = require('moment');
 
 var server = express();
 var port = process.env.PORT || 8080;
-
+server.get('/', function(req, res){
+    res.setHeader('Content-Type', 'text/html');
+    res.send('Timestamp api');
+})
 server.get('/:str', function(req,res) {
     var infoDecoded = urlencode.decode(req.params.str);
     var time, natural;
@@ -13,9 +16,11 @@ server.get('/:str', function(req,res) {
         time = new Date(infoDecoded * 1000);
         natural = moment(time).format('MMMM D, YYYY');
         res.send(JSON.stringify({unix: Number(infoDecoded), natural: natural}));
-    } else {
+    } else if (/^[a-zA-Z]+\s*[0-9]{1,2},\s*[0-9]{1,4}$/.test(infoDecoded)){
         time = new Date(infoDecoded).valueOf()/1000;
         res.send(JSON.stringify({unix: time, natural: infoDecoded}));
+    } else {
+        res.send(JSON.stringify({unix: null, natural: null}));
     }
 })
 server.listen(port, function(){
