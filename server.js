fe6f12cb8ef8af43d7c54c74a7734f9fc1928257
Chen-Hui Chang <j4demacia@gmail.com>
2017-03-21 06:49:41 +0000

    response write header

--- server.js
+++ server.js
@@ -8,13 +8,14 @@ var port = process.env.port || 8080;
 server.get('/:str', function(req,res) {
     var infoDecoded = urlencode.decode(req.params.str);
     var time, natural;
+    res.setHeader('Content-Type', 'application/json');
     if (/^[0-9]+$/.test(infoDecoded)) {
         time = new Date(infoDecoded * 1000);
         natural = moment(time).format('MMMM D, YYYY');
-        res.send({unix: Number(infoDecoded), natural: natural});
+        res.send(JSON.stringify({unix: Number(infoDecoded), natural: natural}));
     } else {
         time = new Date(infoDecoded).valueOf()/1000;
-        res.send({unix: time, natural: infoDecoded});
+        res.send(JSON.stringify({unix: time, natural: infoDecoded}));
     }
 })
 server.listen(port, function(){
